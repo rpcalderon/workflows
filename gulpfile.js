3b86ef77b0241d72d43f790fb243883299460b68
Rosita Calderon <rpcalderon86@gmail.com>
2016-08-12 23:30:47 -0400

    added gulpif and minified production javascript

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp = require('gulp'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
 	connect = require('gulp-connect');
+	gulpif = require('gulp-if');
+	uglify = require('gulp-uglify');
 
 var env,
 	coffeeSources,
@@ -47,6 +49,7 @@ gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
+		.pipe(gulpif(env === 'production', uglify()))
 		.pipe(gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 });
